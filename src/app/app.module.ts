54fdaaf6dd73cbd6ebb86e70935a32cb5a6cf376
gustavo-gamboa <gustavo.gamboa@allianzdirect.es>
2022-01-18 16:05:27 +0100

    primer componente de nuestra aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalViewComponent } from './views/principal-view/principal-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalViewComponent
   ],
   imports: [
     BrowserModule,
