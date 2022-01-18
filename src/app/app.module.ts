181de8e494428af405eb2fecf5501a55346aa852
gustavo-gamboa <gustavo.gamboa@allianzdirect.es>
2022-01-18 16:09:54 +0100

    creado componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalViewComponent } from './views/principal-view/principal-view.component';
+import { LoginViewComponent } from './views/login-view/login-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalViewComponent
+    PrincipalViewComponent,
+    LoginViewComponent
   ],
   imports: [
     BrowserModule,
