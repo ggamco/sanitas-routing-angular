5db43c45b1e6d4f52fde0e69c71b3b09d58c84ba
gustavo-gamboa <gustavo.gamboa@allianzdirect.es>
2022-01-18 16:09:07 +0100

    añadido primer path para visualizar el componente inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalViewComponent } from './views/principal-view/principal-view.component';
 
 const routes: Routes = [
-  
+  {path: "", component: PrincipalViewComponent}
 ];
 
 @NgModule({
